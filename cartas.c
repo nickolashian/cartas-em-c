9c6d826303590d4da867cf7e43575572d3fda673
nickolashian <nickolas.hian@gmail.com>
2025-09-26 17:14:37 -0300

    Add files via upload
    trabalho cartas super trunfo

---
+++ cartas.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main (){
+
+    char estado1;
+    char estado2;
+    char [20] letra1; 
+    char [20] letra2; 
+    char [20] cidade1; 
+    char [20] cidade2; 
+    int habitantes1;
+    int habitantes2;
+    float km1;
+    float km2;
+    float pib1;
+    float pib2;
+    int pontos1;
+    int pontos2;
+
+    scanf
+
+
+
+
+
+
+
+
+
+
+
+}
